293dcb687f87e7ee3492768123646c1a8ffc47b6
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-07-10 00:29:40 -0700

    navigator share testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,17 @@ import './Header.scss'
 import logo from '../img/img.png'
 
 const Header = () => {
-  return (
-    <header className="header">
+    const share = async ( title, text, url ) => {
+        try {
+            await navigator.share({ title: title, text: text, url: url })
+            console.log('o\'zbekiston')
+        } catch (err) {
+            console.log('err: ', err)
+        }
+    }
+
+    return (
+        <header className="header">
             <img src={logo} alt="logo by Lazizbek" className="logo" />
 
             <form action="#" className="search">
@@ -14,7 +23,7 @@ const Header = () => {
 
                 <button className="search__btn"><BsSearch /></button>
             </form>
-            
+
             <nav className="user-nav">
                 <div className="user-nav__icon-box">
                     <BsBookmarksFill size={'2rem'} />
@@ -24,13 +33,13 @@ const Header = () => {
                     <AiOutlineWechat size={'3rem'} />
                     <span>{Math.floor(Math.random() * (79 - 42) + 42)}</span>
                 </div>
-                <div className="user-nav__user">
+                <div className="user-nav__user" onClick={() => share('Share with your fiends', 'LazizbekDevs portfolio', "https://laziz-dev.netlify.app")}>
                     <img src={'https://source.unsplash.com/random/300x300?person,girl,boy,uzbekistan&sig=45'} alt="user avatar lazizbek" className="user" />
                     <span className="user-nov__user-name">Kim Dir</span>
                 </div>
             </nav>
         </header>
-  )
+    )
 }
 
 export default Header
